da77e33f8bdf2e6601d1cc10393d02f1128de354
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-29 02:27:41 -0700

    Creating New Component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import './App.css';
 class App extends Component {
   render(){
     return (
-        <div>
+        <div className="App">
             <h1>Hi, I am Creating React App</h1>
+            <h1>Is it work?</h1>
         </div>
+        
     );
   }
 }
