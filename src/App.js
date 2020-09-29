0260f650e78242000ae80468c8ccca7b344fff56
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-09-29 03:44:14 -0700

    Pass props between the opening and closing tags

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <div className="App">
             <h1>Hi, I am Creating React App</h1>
             <h1>Is it work?</h1>
-            <Person name = "Dhrubo" age = "20"></Person>
+            <Person name = "Dhrubo" age = "20">What are you doing</Person>
             <Person name = "Dhruboish" age = "21"></Person>
             <Person name = "Amdadul" age = "19"></Person>
             <Person name = "Solimuddin" age = "55"></Person>
--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -5,6 +5,7 @@ const Person = (props) => {
         <div>
             <div>
                 <h1>I am {props.name} and I am {props.age} years old</h1>
+                <h3>{props.children}</h3>
             </div>
         </div>
     );
